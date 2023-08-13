220392dcef9d10054596a1e1e3dcc0ed523394c4
VovaMolxs <molxs@outlook.com>
2023-08-13 21:42:07 +0300

    add Transaction

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
     public function index() {
         //dd('test');
+
         return view('admin.index');
     }
 }
